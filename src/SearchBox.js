184a0449b8c38c44d46913d60bd139f34451f134
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-04-17 06:52:50 +0800

    add search event

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBox = ({searchField, searchChange}) => {
+    return(
+    <div className="pa2">
+        <input className="pa3 ba b--green bg-lighest-blue" type="search" placeholder="search robots" onChange={searchChange}/>
+    </div>
+    );
+};
+
+export default SearchBox;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "tachyons";
-
-import Card from "./Card";
-import { robots } from "./robots";
+import App from "./App";
+// import CardList from "./CardList";
+// import { robots } from "./robots";
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -12,9 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+        <App />
     </React.StrictMode>
 );
 
